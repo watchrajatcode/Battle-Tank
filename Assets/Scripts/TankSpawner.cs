bd147252133ea16da292e33b359927bbf9b50158
Rajat_the_HERO <makegamewithrajat@gmail.com>
2022-07-15 10:57:22 +0530

    UI Selection Panel
    made UI for selecting Tanks

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -17,17 +17,31 @@ public class TankSpawner : MonoBehaviour
 
 
     public TankView tankView;
+
     // Start is called before the first frame update
     void Start()
     {
-        CreateTank();   
+        
     }
     
-    private void CreateTank()
-    {
-        TankModel tankModel = new TankModel(tanklist[1].movementSpeed, tanklist[1].rotationSpeed, tanklist[1].tankType, tanklist[1].color);
-        TankController tankController = new TankController(tankModel, tankView);
 
+    
+    public void CreateTank(TankTypes tankType)
+    {
+        if (tankType == TankTypes.BlueTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[1].movementSpeed, tanklist[1].rotationSpeed, tanklist[1].tankType, tanklist[1].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        } else if (tankType == TankTypes.RedTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[2].movementSpeed, tanklist[2].rotationSpeed, tanklist[2].tankType, tanklist[2].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        else
+        {
+            TankModel tankModel = new TankModel(tanklist[0].movementSpeed, tanklist[0].rotationSpeed, tanklist[0].tankType, tanklist[0].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
     }
 
 }
