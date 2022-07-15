06a8f038310fb195831b66085a769708fa811147
Rajat_the_HERO <makegamewithrajat@gmail.com>
2022-07-15 09:21:21 +0530

    Camera , Tank movement and types
    Camera movement, tank movement and different tank types

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -7,15 +7,32 @@ public class TankController
 
     private TankView tankView;
 
+    private Rigidbody rb;
+
     public TankController(TankModel _tankModel, TankView _tankView)
     {
         tankModel = _tankModel;
-        tankView = _tankView;
+        tankView = GameObject.Instantiate<TankView>(_tankView);
+        rb = tankView.GetRigidbody();
+
         tankModel.SetTankController(this);
         tankView.SetTankController(this);
 
-        GameObject.Instantiate(tankView.gameObject);
+        tankView.ChangeColor(tankModel.color);
+    }
+    public void Move(float movement, float movementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * movement * movementSpeed;
+    }
+    public void Rotate(float rotate, float rotateSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotate * rotateSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 
-    
+    public TankModel GetTankModel()
+    {
+        return tankModel;
+    }
 }
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
+    public List<Tank> tanklist;
+    [System.Serializable]
+    public class Tank
+    {
+        public float movementSpeed;
+        public float rotationSpeed;
+        public TankTypes tankType;
+        public Material color;
+    }
+    
+
+
     public TankView tankView;
     // Start is called before the first frame update
     void Start()
     {
         CreateTank();   
     }
-
+    
     private void CreateTank()
     {
-        TankModel tankModel = new TankModel();
+        TankModel tankModel = new TankModel(tanklist[1].movementSpeed, tanklist[1].rotationSpeed, tanklist[1].tankType, tanklist[1].color);
         TankController tankController = new TankController(tankModel, tankView);
 
     }
