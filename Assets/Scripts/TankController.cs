728ec61743596c16d8f85ea06cca62532193f631
Rajat_the_HERO <makegamewithrajat@gmail.com>
2022-07-15 05:38:33 +0530

    set up MVC architechture
    Setting up MVC

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class TankController
+{
+    private TankModel tankModel;
+
+    private TankView tankView;
+
+    public TankController(TankModel _tankModel, TankView _tankView)
+    {
+        tankModel = _tankModel;
+        tankView = _tankView;
+        tankModel.SetTankController(this);
+        tankView.SetTankController(this);
+
+        GameObject.Instantiate(tankView.gameObject);
+    }
+
+    
+}
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-    public GameObject tankPrefab;
+    public TankView tankView;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(tankPrefab, transform.position, Quaternion.identity);   
+        CreateTank();   
     }
 
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankView);
+
+    }
 
 }
